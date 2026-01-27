ea02a015847be3ae6f84303ce92fa61498508363
kengrenokRu <nastassiadunaeva@gmail.com>
2026-01-27 22:05:43 +0300

    init: init new project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+
+function App() {
+  return (   
+      <header className="App-header">       
+        <p>
+          Hello React!
+        </p>        
+      </header>   
+  );
+}
+
+export default App;
